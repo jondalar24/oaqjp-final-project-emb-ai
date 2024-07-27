a2cb5a866986314735c458df687c0a0db63b4bfc
jondalar24 <jondalar24@msn.com>
2024-07-27 05:57:24 -0400

    cambios hechos

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,10 +1,7 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyze):   
-    if not text_to_analyze:
-        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
-    
+def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -23,5 +20,14 @@ def emotion_detector(text_to_analyze):
         'sadness': emotions['sadness'],
         'dominant_emotion': dominant_emotion
     }
-    
-    return formatted_response
+    if response.status_code == 200:
+        return formatted_response
+    elif response.status_code == 400:
+        return {
+            'anger':None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
--- server.py
+++ server.py
@@ -1,1 +1,29 @@
-f
+''' Executing this function initiates the application of emorion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector", methods = ['GET'])
+def sent_detection():
+    text_to_analyze = request.args.get('textToAnalyze')
+    if text_to_analyze and text_to_analyze.strip():
+        response = emotion_detector(text_to_analyze)
+        formatted_response = (
+            f"For the given statement, the system response is 'anger': {response['anger']}, "
+            f"'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} "
+            f"and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+        )
+        return formatted_response
+    else:
+        return "Invalid text! Please try again!", 400
+    
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0", port = 5000)
